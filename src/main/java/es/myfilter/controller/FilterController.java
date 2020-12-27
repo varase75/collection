eb4f1928f163146eb26254c35ff82614388b89bb
Eugenio Varas <57813050+varase75@users.noreply.github.com>
2020-12-27 11:34:15 +0100

    Add files via upload

--- src/main/java/es/myfilter/controller/FilterController.java
+++ src/main/java/es/myfilter/controller/FilterController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import es.myfilter.domain.ItemDto;
 import es.myfilter.service.CollectionService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
@@ -23,14 +24,14 @@ public class FilterController {
         this.collectionHandlerPort = collectionHandlerPort;
     }
 
-    @ApiOperation(value = "View List items filter", response = ItemDto.class, responseContainer = "List")
+    @ApiOperation(value = "Recuperar una lista con todos los datos filtrados", response = ItemDto.class, responseContainer = "List")
 	@ApiResponses(value = {
 		@ApiResponse(code = 200, message = "Successfully retrieved items"),
-		@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-		@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-		@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+		@ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+		@ApiResponse(code = 500, message = "Internal error")
+	})
     @GetMapping("/all")
-    public List<ItemDto> getCollection(@RequestParam(value = "filter", required = false, defaultValue = "") String filter) {
+    public List<ItemDto> getCollection(@ApiParam(value = "Filtro aplicado", required = false) @RequestParam(value = "filter", required = false, defaultValue = "") String filter) {
     	return collectionHandlerPort.getCollection(filter);
     }
 
